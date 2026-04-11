ba7cda50ed52a7d2c6156a39ade594243a8ed8c5
Klajd Belishaku <devklajd@proton.me>
2026-04-11 02:28:19 +0200

    next function fix

--- backend/models/BusinessUser.js
+++ backend/models/BusinessUser.js
@@ -13,10 +13,9 @@ const businessUserSchema = new mongoose.Schema({
   updatedAt: { type: Date, default: Date.now },
 });
 
-businessUserSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
+businessUserSchema.pre('save', async function () {
+  if (!this.isModified('password')) return;
   this.password = await bcrypt.hash(this.password, 10);
-  next();
 });
 
 businessUserSchema.pre('save', function (next) {
